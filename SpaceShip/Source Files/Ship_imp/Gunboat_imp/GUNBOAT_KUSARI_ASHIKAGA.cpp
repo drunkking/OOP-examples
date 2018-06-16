586023c8cd3a7623a281946abe322d633f7da5d4
Dušan <34519456+drunkking@users.noreply.github.com>
2018-06-16 20:18:06 +0200

    Add files via upload

---
+++ SpaceShip/Source Files/Ship_imp/Gunboat_imp/GUNBOAT_KUSARI_ASHIKAGA.cpp
@@ -0,0 +1,59 @@
+#include "Gunboat_Kusari_Ashikaga.h"
+
+Gunboat_Kusari_Ashikaga::Gunboat_Kusari_Ashikaga() :
+	hull_strength_(115000),
+	max_shield_class_(5),
+	cargo_space_(350),
+	max_impulse_speed_(80),
+	price_(28000000) {
+
+}
+
+Gunboat_Kusari_Ashikaga::Gunboat_Kusari_Ashikaga(const std::string& faction):
+	SpaceShip(faction),
+	hull_strength_(115000),
+	max_shield_class_(5),
+	cargo_space_(350),
+	max_impulse_speed_(80),
+	price_(28000000) {
+
+}
+
+void Gunboat_Kusari_Ashikaga::SetHullStrength(const double& hull_strength) {
+	hull_strength_ = hull_strength;
+}
+
+double Gunboat_Kusari_Ashikaga::GetHullStrength() const {
+	return hull_strength_;
+}
+
+void Gunboat_Kusari_Ashikaga::SetMaxShiledClass(const int& max_shield_class) {
+	max_shield_class_ = max_shield_class;
+}
+
+int Gunboat_Kusari_Ashikaga::GetMaxShieldClass() const {
+	return max_shield_class_;
+}
+
+int Gunboat_Kusari_Ashikaga::GetCargoSpace() const {
+	return cargo_space_;
+}
+
+int Gunboat_Kusari_Ashikaga::GetMaxImpulseSpeed() const {
+	return max_impulse_speed_;
+}
+
+int Gunboat_Kusari_Ashikaga::GetPrice() const {
+	return price_;
+}
+
+std::ostream& operator<< (std::ostream& out, const Gunboat_Kusari_Ashikaga& ship) {
+	out << "\n";
+	out << "Name: " << "Gunboat Kusari Ashikaga" << "\n";
+	out << "Hull strength: " << ship.GetHullStrength() << "\n";
+	out << "Max Shield class: " << ship.GetMaxShieldClass() << "\n";
+	out << "Cargo space: " << ship.GetCargoSpace() << "\n";
+	out << "Max Impulse speed: " << ship.GetMaxImpulseSpeed() << "\n";
+	out << "Price: " << ship.GetPrice() << "\n";
+	return out;
+}
