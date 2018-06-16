14fc139ae83541a44827f8f52dcef42606fd46be
Dušan <34519456+drunkking@users.noreply.github.com>
2018-06-16 20:02:47 +0200

    Create Fighter_MJ1_Gecko_Council.h

---
+++ SpaceShip/Header Files/Ships/Fighter/Fighter_MJ1_Gecko_Council.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "SpaceShip.h"
+
+class Fighter_MJ1_Gecko_Council : public SpaceShip {
+public:
+	Fighter_MJ1_Gecko_Council();
+	Fighter_MJ1_Gecko_Council(const std::string&);
+
+	void SetHullStrength(const double&);
+	double GetHullStrength() const;
+
+	void SetMaxShiledClass(const int&);
+	int GetMaxShieldClass() const;
+
+	int GetCargoSpace() const;
+	int GetMaxImpulseSpeed() const;
+	int GetPrice() const;
+
+	friend std::ostream& operator<< (std::ostream&, const Fighter_MJ1_Gecko_Council&);
+private:
+	double hull_strength_;
+	int max_shield_class_;
+	int cargo_space_;
+	int max_impulse_speed_;
+	int price_;
+};
