696177a4dd3a6d3d50a96026a894e8782ecaccab
Dušan <34519456+drunkking@users.noreply.github.com>
2018-06-16 20:01:58 +0200

    Add files via upload

---
+++ SpaceShip/Header Files/Ships/Capital/Capital_RHGM482_Red_Hessian_Cruiser.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "SpaceShip.h"
+
+class Capital_RHGM482_Red_Hessian_Cruiser : SpaceShip {
+public:
+	Capital_RHGM482_Red_Hessian_Cruiser();
+	Capital_RHGM482_Red_Hessian_Cruiser(const std::string&);
+
+	void SetHullStrength(const double&);
+	double GetHullStrength() const;
+
+	void SetMaxShiledClass(const int&);
+	int GetMaxShieldClass() const;
+
+	int GetCargoSpace() const;
+	int GetMaxImpulseSpeed() const;
+	int GetPrice() const;
+
+	friend std::ostream& operator<< (std::ostream&, const Capital_RHGM482_Red_Hessian_Cruiser&);
+private:
+	double hull_strength_;
+	int max_shield_class_;
+	int cargo_space_;
+	int max_impulse_speed_;
+	int price_;
+};
